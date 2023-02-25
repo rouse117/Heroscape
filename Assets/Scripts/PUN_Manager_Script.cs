5f41ba3b2a73f940ad07241e4225eeeba668f3bf
Jimmy Lib <jameselliott2010@gmail.com>
2023-02-24 18:13:15 -0600

    Cube Prototype

--- Assets/Scripts/PUN_Manager_Script.cs
+++ Assets/Scripts/PUN_Manager_Script.cs
@@ -2,24 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
+using Photon.Realtime;
 
 
 public class PUN_Manager_Script : MonoBehaviourPunCallbacks
 {
+    private const string ROOM_NAME = "myRoom";
+    private const int MAX_PLAYERS = 2;
 
     string gameVersion = "1";
-    
+
     private void Start() {
         Connect();
     }
 
-public void Connect()
+    public void Connect()
     {
         // we check if we are connected or not, we join if we are , else we initiate the connection to the server.
         if (PhotonNetwork.IsConnected)
         {
             // #Critical we need at this point to attempt joining a Random Room. If it fails, we'll get notified in OnJoinRandomFailed() and we'll create one.
-            PhotonNetwork.JoinRandomRoom();
+            PhotonNetwork.JoinOrCreateRoom(ROOM_NAME, new RoomOptions { MaxPlayers = MAX_PLAYERS }, TypedLobby.Default);
         }
         else
         {
@@ -28,10 +31,27 @@ public void Connect()
             PhotonNetwork.GameVersion = gameVersion;
         }
     }
+
+
     public override void OnConnectedToMaster()
     {
-        Debug.Log("OnConnectedToMaster() was called by PUN.");
-        PhotonNetwork.JoinRandomRoom();
+        PhotonNetwork.JoinOrCreateRoom(ROOM_NAME, new RoomOptions { MaxPlayers = MAX_PLAYERS }, TypedLobby.Default);
     }
-    // ...
+
+    public override void OnJoinedRoom()
+    {
+        int playerValue = PhotonNetwork.CurrentRoom.PlayerCount;
+
+        if (playerValue == 1)
+        {
+            Debug.Log("You are Player 1");
+        }
+        else if (playerValue == 2)
+        {
+            Debug.Log("You are Player 2");
+        }
+    }
 }
+
+
+
